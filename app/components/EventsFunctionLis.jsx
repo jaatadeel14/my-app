f74177e5153d9cb4a00a1368587ab3af532527d4
Adeel Shabbir <jaatadeel6@gmail.com>
2025-10-23 01:07:46 +0300

    ok

---
+++ app/components/EventsFunctionLis.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const EventsFunctionLis = () => {
+    const [firstName,setFirstName]=useState("AdeEl")
+
+    // const sayHello=(name)=>{
+    //     alert(`Hello ${name}`)
+    // }
+    const changeName=()=>{
+        setFirstName("Jaat")
+    }
+
+  return (
+    <>
+        <h1>Hello this is Adeel Jaat</h1>
+        {/* <button onClick={()=> sayHello("Adeel")}>Click me</button> */}
+        <h3>My name is : {firstName}</h3>
+        <button onClick={changeName}>Click me</button>
+
+    </>
+  )
+}
+
+export default EventsFunctionLis
