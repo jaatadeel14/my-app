a05911276d2d4425140e1529886375df2a4b64c2
Adeel Shabbir <jaatadeel6@gmail.com>
2025-10-29 00:28:35 +0300

    ok

---
+++ app/blog/[...posts]/page.js
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react';
+
+const Posts = ({ params }) => {
+  const resolvedParams = React.use(params);
+
+  return (
+    <>
+      <h2>Blog: {resolvedParams.posts[0]}</h2>
+      <h2>Post: {resolvedParams.posts[1]}</h2>
+    </>
+  );
+};
+
+export default Posts;
---
+++ app/home/[...pageNotFound]/page.js
@@ -0,0 +1,11 @@
+const Routebase = () => {
+    return (
+        <>
+
+            <h2>404 Page | Page Not Found home page</h2>
+
+        </>
+    )
+}
+
+export default Routebase
