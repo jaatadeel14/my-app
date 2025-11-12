e8a173157f9f9902bc8f2d9e657f5b3b14e9d69c
Adeel Shabbir <jaatadeel6@gmail.com>
2025-11-12 22:50:10 +0300

    alla

--- app/api/employee/[empid]/route.js
+++ app/api/employee/[empid]/route.js
@@ -10,6 +10,23 @@
 //     return NextResponse.json(result)
 // }
 
+
+// --------------------------------------------------------------------------------------------
+
+
+
+// import { employee } from "@/app/db/db";
+// import { NextResponse } from "next/server";
+
+// export const GET = (req, value) => {
+//     // const { empid } = await value.params; // ✅ await required
+//     // console.log(empid);
+//     // const data = employee;
+//     const empData = employee.filter((item) => item.employeeId == value.params.empid)
+//     let result = empData.length == 0 ? { result: 'No Employee Found' } : { result: empData }
+//     return NextResponse.json(result)
+// }
+
 import { employee } from "@/app/db/db";
 import { NextResponse } from "next/server";
 
