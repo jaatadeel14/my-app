837b21d7f1e371f0cb4dfa1da65954f413878c94
Adeel Shabbir <jaatadeel6@gmail.com>
2025-11-13 23:05:06 +0300

    lahu

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 export const POST = async (req, res) => {
     let data = await req.json();
     console.log(data)
-    if(!data.id || !data.salary){
+    if(!data.username || !data.age){
         return NextResponse.json({result:"Bad Request"},{status:400})
     }
     // return NextResponse.json({ name: "Adeel", age: 23 })
