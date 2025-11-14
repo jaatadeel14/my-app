aae995032d8b60723a58a6db0bcb2b6f249756e5
Adeel Shabbir <jaatadeel6@gmail.com>
2025-11-14 22:21:54 +0300

    acha howa

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -9,3 +9,10 @@ export const POST = async (req, res) => {
     // return NextResponse.json({ name: "Adeel", age: 23 })
      return NextResponse.json({result:"Success" },{status:200})
 }
+
+
+
+
+
+
+// This API route receives student data from the client. It reads the JSON body, checks that both username and age are provided, and returns a 400 Bad Request response if any field is missing. If all required data is present, it returns a 200 Success response.
